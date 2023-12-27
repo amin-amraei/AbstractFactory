a978920d6213269abb6c6329745624a9ee7eb92f
a.amraei <amin.amraei@gmail.com>
2023-12-27 09:39:00 +0330

    Add project files.

---
+++ ConcreteFactory1.cs
@@ -0,0 +1,22 @@
+﻿using AbstractFactory.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class ConcreteFactory1 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+}
---
+++ ConcreteProductB2.cs
@@ -0,0 +1,24 @@
+﻿using AbstractFactory.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class ConcreteProductB2 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+    }
+}
